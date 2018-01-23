be5c07cf8a0eb15091f1c1b58dc81bc324263e20
moxdev <moxdev1@gmail.com>
2018-01-23 17:48:15 -0500

    finsihed owner section

--- inc/home-ownership.php
+++ inc/home-ownership.php
@@ -1,18 +1,50 @@
 <?php
 /**
  * Custom function for homepage
- * 
+ *
  * @package Atlantic Cruising Yachts
  */
 
 function atlantic_cruising_yachts_homepage_ownership_section() {
-  if ( function_exists( 'get_field' ) ) { ?>
+  if ( function_exists( 'get_field' ) ) {
+    $img = get_field('home_page_split_section_background_image');
+    $title = get_field('home_page_split_section_title');
+    $content = get_field('home_page_split_section_content'); ?>
 
-    <section class="home-ownership">
+    <?php if ($title): ?>
 
-      <h1>Ownership</h1>
+      <section class="home-ownership">
+        <div class="home-ownership-wrapper">
 
-    </section>
+          <?php if ($img): ?>
+
+            <img src="<?php echo esc_url( $img['sizes']['large'] ); ?>" alt="<?php echo esc_attr( $img['alt'] ); ?>" description="<?php echo esc_attr( $img['description'] ); ?>">
+
+          <?php endif ?>
+
+          <div class="home-ownership-inner-wrapper">
+
+            <?php if ($title): ?>
+
+              <h2><?php echo esc_html( $title ); ?></h2>
+
+            <?php endif ?>
+
+            <?php if ($content): ?>
+
+              <div class="content-wrapper">
+                <?php echo $content ?>
+              </div>
+
+            <?php endif ?>
+
+          </div>
+          <div class="overlay"></div>
+
+        </div>
+      </section>
+
+    <?php endif ?>
 
   <?php
 
