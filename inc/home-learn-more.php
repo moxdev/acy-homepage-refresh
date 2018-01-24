eb9675755520a4993398ddb91e952315054b9211
moxdev <moxdev1@gmail.com>
2018-01-24 16:00:09 -0500

    added dynamic colum support

--- inc/home-learn-more.php
+++ inc/home-learn-more.php
@@ -7,10 +7,12 @@
 
 function atlantic_cruising_yachts_homepage_learn_more_section() {
   if ( function_exists( 'get_field' ) ) {
-    if( have_rows('home_page_sub_content') ) { ?>
+    if( have_rows('home_page_sub_content') ) {
+
+      $count   = count(get_field('home_page_sub_content')); ?>
 
     <section class="home-learn-more">
-      <div class="home-sub-content-wrapper">
+      <div class="home-sub-content-wrapper col-<?php echo $count; ?>">
         <?php while ( have_rows('home_page_sub_content') ) : the_row();
           $title   = get_sub_field('sub_content_title');
           $img     = get_sub_field('sub_content_image');
