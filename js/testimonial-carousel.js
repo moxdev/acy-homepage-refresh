127e0f311a48fa3365e4d97a2667609a76e50381
moxdev <moxdev1@gmail.com>
2018-02-14 15:06:18 -0500

    broke up sass and made dits to testimonial section

--- js/testimonial-carousel.js
+++ js/testimonial-carousel.js
@@ -1,12 +1,13 @@
 // Carousel
-var elem = document.querySelector(".testimonial-carousel");
+var elem = document.querySelector('.testimonial-carousel');
 var flkty = new Flickity(elem, {
-  cellAlign: "left",
+  cellAlign: 'left',
   contain: true,
   pageDots: false,
   setGallerySize: true,
   groupCells: true,
   autoPlay: 7000,
+  // autoPlay: false,
   resize: true,
   selectedAttraction: 0.01,
   friction: 0.2,
